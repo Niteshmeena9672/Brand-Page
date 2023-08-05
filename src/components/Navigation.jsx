7c327c17fd450c1dc6953a5e529447be56d88ce0
nitesh <niteshmeena9672@gmail.com>
2023-08-05 13:35:15 +0530

    favicon + title update

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,33 @@
+// The Navigation component represents the navigation bar of the application
 const Navigation = () => {
   return (
+    // The nav element with a container class to style the navigation bar
     <nav className="container">
-        <div className="logo">
-          <img src="./public/images/brand_logo.png" alt="brand logo" />
-        </div>
-        <ul>
-          <li href="#">Menu</li>
-          <li href="#">Location</li>
-          <li href="#">About</li>
-          <li href="#">Contact</li>
-        </ul>
-        <button>login</button>
-      </nav>
+      {/* The logo section with a brand logo image */}
+      <div className="logo">
+        <img src="./public/images/brand_logo.png" alt="brand logo" />
+      </div>
+
+      {/* The unordered list to display navigation links */}
+      <ul>
+        {/* Navigation link for the "Menu" */}
+        <li href="#">Menu</li>
+
+        {/* Navigation link for the "Location" */}
+        <li href="#">Location</li>
+
+        {/* Navigation link for the "About" */}
+        <li href="#">About</li>
+
+        {/* Navigation link for the "Contact" */}
+        <li href="#">Contact</li>
+      </ul>
+
+      {/* Button for user login */}
+      <button>login</button>
+    </nav>
   );
 }
+
+// Export the Navigation component to make it available for other parts of the application
 export default Navigation;
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@
 import React from 'react'; // Import the React library
 import ReactDOM from 'react-dom/client'; // Import the ReactDOM library
 import App from './App.jsx'; // Import the main application component
-import './index.css'; // Import the CSS file for styling
 
 // Use ReactDOM's createRoot function to render the application
 ReactDOM.createRoot(document.getElementById('root')).render(
