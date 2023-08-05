87c1295f47a33e15e9fc5b2a5e5c27897a2bc603
nitesh <niteshmeena9672@gmail.com>
2023-08-05 13:23:22 +0530

    main section completed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// Import necessary dependencies
+import React from 'react'; // Import the React library
+import ReactDOM from 'react-dom/client'; // Import the ReactDOM library
+import App from './App.jsx'; // Import the main application component
+import './index.css'; // Import the CSS file for styling
 
+// Use ReactDOM's createRoot function to render the application
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // Use React.StrictMode for development mode checks and warnings
   <React.StrictMode>
+    {/* Render the main application component */}
     <App />
   </React.StrictMode>,
-)
+);
