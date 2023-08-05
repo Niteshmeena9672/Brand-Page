cf7091060fa0fd059455adcdbb05d6f27a89f783
nitesh <niteshmeena9672@gmail.com>
2023-08-05 13:22:32 +0530

    App section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css';
 import Navigation from './components/Navigation.jsx';
 import HeroSection from './components/Hero.jsx';
+
+// The main component for the application
 const App = () => {
   return (
     <div>
+      {/* Render the navigation component */}
       <Navigation />
+
+      {/* Render the hero section component */}
       <HeroSection />
     </div>
-  )
+  );
 }
+
 export default App;
